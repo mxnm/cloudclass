40b01f0e9075f432bf56eebc7e36d45c9173ebfc
Milan Nankov <milan@nankov.com>
2018-04-14 15:11:45 +0300

    Added Cor project for storage

---
+++ azure-storage-core/BlobStorage/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace BlobStorage
+{
+    class Program
+    {
+        public static async Task MainAsync(string[] args)
+        {
+             // 01 - Connect to your azure storage account
+            var storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=fdfgdfgdfgd;AccountKey=jEyGRzzyuf0hdXb9EgXyftmqWqBHl8xpxdXqmxbonjco+R2VuldtaC5zP9VLXoF2sEq9nnhJ7rPLTS/NEoFSJA==;EndpointSuffix=core.windows.net");
+            var client = storageAccount.CreateCloudBlobClient();
+
+            // 02 - Create a container called "text-files"
+            var container = client.GetContainerReference("text-files");
+            await container.CreateIfNotExistsAsync();
+
+            // 03 - Set the container permissions to BlobContainerPublicAccessType.Blob
+            await container.SetPermissionsAsync(new BlobContainerPermissions() { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+            // 04 - Upload SampleText.txt to a block block called "UploadedSampleText.txt"
+            var bytes = File.ReadAllBytes("SampleText.txt");
+            var blob = container.GetBlockBlobReference("UploadedSampleText.txt");
+            await blob.UploadFromByteArrayAsync(bytes, 0, bytes.Length);
+
+            // 05 - Download "UploadedSampleText.txt" from the storage account and print its contents using Console.WriteLine()
+            var downloadStream = new MemoryStream();
+            blob = container.GetBlockBlobReference("UploadedSampleText.txt");
+            await blob.DownloadToStreamAsync(downloadStream);
+            downloadStream.Position = 0;
+            var reader = new StreamReader(downloadStream);
+
+            Console.WriteLine(reader.ReadToEnd());
+
+            // 05 - Delete UploadedSampleText.txt from the storage
+            await blob.DeleteAsync();
+        }
+        static void  Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ azure-storage-core/TableStorage/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TableStorage
+{
+    class Program
+    {
+        public static async Task MainAsync(string[] args)
+        {
+             // 01 - Connect to your azure storage account
+            var storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=aubgtestatorage;AccountKey=8MktAwg721zQRBK4ZMUxGJvvvMjq0WslLkilbnjndbf96yS5or3PhvoZTpruX7mTIeQNBaE5YgSkhJBBCiG7jg==;");
+            var client = storageAccount.CreateCloudTableClient();
+
+            // 02 - Create a table called "customers"
+            var table = client.GetTableReference("customers");
+            await table.CreateIfNotExistsAsync();
+
+            // 03 - Insert single entity (instance of the CustomerEntity class) into the table
+            var customer1 = new CustomerEntity("Bulgaria", "Maria");
+
+            var insertOperation = TableOperation.Insert(customer1);
+            await table.ExecuteAsync(insertOperation);
+
+            // 04 - Insert two additional CustomerEntity objects using batching (use PartitionKey "Netherlands")
+            var customer2 = new CustomerEntity("Netherlands", "Thijs");
+            var customer3 = new CustomerEntity("Netherlands", "OtherGuy");
+
+            var batchOperation = new TableBatchOperation();
+            batchOperation.Insert(customer2);
+            batchOperation.Insert(customer3);
+            await table.ExecuteBatchAsync(batchOperation);
+
+            // 05 - Retrieve one of the entities using TableOperation.Retrieve and print its PartitionKey using Console.WriteLine()
+            var retrieve = TableOperation.Retrieve<CustomerEntity>("Bulgaria", "Maria");
+            var singleResult = (await table.ExecuteAsync(retrieve)).Result as CustomerEntity;
+            
+            Console.WriteLine(singleResult.PartitionKey);
+
+            // 07 - Delete one of the entities from the table 
+            var deleteOperation = TableOperation.Delete(singleResult);
+            await table.ExecuteAsync(deleteOperation);
+
+            // 08 - Insert a new entity into "customers" using DynamicTableEntity instead of CustomerEntity
+            var dynamicCustomer = new DynamicTableEntity("Bulgaria", "Georgi");
+            dynamicCustomer.Properties["NewProperty"] = new EntityProperty("AUBG");
+
+            var insertDynamic = TableOperation.Insert(dynamicCustomer);
+            await table.ExecuteAsync(insertDynamic);
+        }
+        static void  Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+
+            Console.ReadKey();
+        }
+    }
+}
