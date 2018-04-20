194e1a200843f8f62d4e2ac9c33f85f590d07db8
Milan Nankov <milan@nankov.com>
2018-04-21 02:08:08 +0300

    Fixed Error page.

---
+++ text-analysis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace book_exchange_initial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
